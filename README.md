2666c40213faf87a93169bd5e4bc0307dca47081
vic13630 <37115306+vic13630@users.noreply.github.com>
2019-06-26 21:44:51 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Area_Calculation
+
+計算面積測試用Micro:bit積木
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+Area_Calculation=github:vic13630/Area_Calculation
+```
---
+++ main.ts
@@ -0,0 +1,24 @@
+/** * 計算長方形面積 */
+
+//% weight=0 color=##00FF00 icon="\uf1fe" block="Area_Calculation"
+
+namespace Area_Calculation
+{
+
+/** * 回傳 */
+//% blockId="ReturnArea" block="Return Area length %length | width %width"
+//% blockGap=2 weight=0 blockExternalInputs=true
+export function ReturnArea(length:number, width:number) : number
+	{
+		return length*width
+	}
+
+/** * 不回傳 */
+//% blockId="ShowArea" block="Show Area length %length | width %width"
+//% blockGap=2 weight=1
+export function ShowArea(length:number, width:number) : void
+	{
+		basic.showNumber(length*width)
+	}
+
+}
