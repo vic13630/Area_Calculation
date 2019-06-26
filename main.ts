c27652470de33777e3c2507576212d5e2c5f69cf
vic13630 <37115306+vic13630@users.noreply.github.com>
2019-06-26 23:24:24 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ namespace Area_Calculation
 /** 
  * 計算長方形面積 & 回傳 
  */
-//% blockId="ReturnAreaOfRectangle" block="Return Area of Rectangle = length %length | * | width %width"
+//% blockId="ReturnAreaOfRectangle" block="Return Area of Rectangle = length %length | x  width %width"
 //% blockGap=5 weight=0
 export function ReturnAreaOfRectangle(length: number, width: number) : number {
 	return length*width
@@ -14,7 +14,7 @@ export function ReturnAreaOfRectangle(length: number, width: number) : number {
 /** 
  * 計算三角形面積 & 回傳 
  */
-//% blockId="ReturnAreaOfTriangle" block="Return Area of Triangle = bottom %bottom | * | high %high | /2"
+//% blockId="ReturnAreaOfTriangle" block="Return Area of Triangle = bottom %bottom | x  high %high | /2"
 //% blockGap=5 weight=1
 export function ReturnAreaOfTriangle(bottom: number, high: number) : number {
 	return bottom*high/2
