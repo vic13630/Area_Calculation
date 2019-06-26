752e0a35bc1cf07b8c4e96b19abfd982965f8bda
vic13630 <37115306+vic13630@users.noreply.github.com>
2019-06-26 22:11:33 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,24 +1,23 @@
-/** * 計算長方形面積 */
-
-//% weight=0 color=#47FF33 icon="\uf1fe" block="Area_Calculation"
-
+//% weight=0 color=#47FF33 icon="\uf1fe"
 namespace Area_Calculation
 {
 
-/** * 回傳 */
+/** 
+ * 計算長方形面積 & 回傳 
+ */
 //% blockId="ReturnArea" block="Return Area length %length | width %width"
-//% blockGap=2 weight=0 blockExternalInputs=true
-export function ReturnArea(length:number, width:number) : number
-	{
-		return length*width
-	}
+//% blockGap=2 weight=0
+export function ReturnArea(length: number, width: number) : number {
+	return length*width
+}
 
-/** * 不回傳 */
+/** 
+ * 計算長方形面積 & 不回傳 
+ */
 //% blockId="ShowArea" block="Show Area length %length | width %width"
 //% blockGap=2 weight=1
-export function ShowArea(length:number, width:number) : void
-	{
-		basic.showNumber(length*width)
-	}
+export function ShowArea(length: number, width: number) : void {
+	basic.showNumber(length*width)
+}
 
 }
