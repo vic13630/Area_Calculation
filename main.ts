3b411debfacad7070e2bcd00847a5d303cf76ac7
vic13630 <37115306+vic13630@users.noreply.github.com>
2019-06-26 23:06:01 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,23 +1,25 @@
-//% weight=0 color=#47FF33 icon="\uf1fe" block="Area_Calculation"
+//% weight=0 color=#47FF33 icon="\uf1fe"
 namespace Area_Calculation
 {
 
 /** 
  * 計算長方形面積 & 回傳 
  */
-//% blockId="ReturnArea" block="Return Area length %length | width %width"
-//% blockGap=2 weight=0
-export function ReturnArea(length: number, width: number) : number {
+//% blockId="ReturnAreaOfRectangle" block="Return Area of Rectangle : length %length | width %width"
+//% blockGap=5 weight=0
+export function ReturnAreaOfRectangle(length: number, width: number) : number {
 	return length*width
 }
 
 /** 
- * 計算長方形面積 & 不回傳 
-
-//% blockId="ShowArea" block="Show Area length %length | width %width"
-//% blockGap=2 weight=1
-export function ShowArea(length: number, width: number) : void {
-	basic.showNumber(length*width)
-}
+ * 計算三角形面積 & 回傳 
  */
+//% blockId="ReturnAreaOfTriangle" block="Return Area of Triangle : bottom %bottom | high %high | /2"
+//% blockGap=5 weight=1
+export function ReturnAreaOfTriangle(bottom: number, high: number) : number {
+	return bottom*high/2
+}
+
+
+
 }
